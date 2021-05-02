dd157cb502fcb10086581168f197a17d10e35cf4
Roman Shtylman <roman@foxglove.dev>
2021-05-02 10:12:26 -0700

    Remove OsContextSingleton from AnalyticsProvider

--- app/context/AnalyticsProvider.tsx
+++ app/context/AnalyticsProvider.tsx
@@ -4,18 +4,25 @@
 
 import { PropsWithChildren, useMemo } from "react";
 
-import OsContextSingleton from "@foxglove-studio/app/OsContextSingleton";
 import AnalyticsContext from "@foxglove-studio/app/context/AnalyticsContext";
 import { Analytics } from "@foxglove-studio/app/services/Analytics";
+import useAppSetting from "@foxglove-studio/app/hooks/useAppSetting";
+import { AppSetting } from "@foxglove-studio/app/AppSetting";
 
 export default function AnalyticsProvider(
   props: PropsWithChildren<{ amplitudeApiKey?: string }>,
 ): React.ReactElement {
+  // fixme - ensure this setting is available and loaded on first render
+  const telemetryEnabled = useAppSetting<boolean>(AppSetting.TELEMETRY_ENABLED);
+  const crashReportingEnabled = useAppSetting<boolean>(AppSetting.CRASH_REPORTING_ENABLED);
+
   const analytics = useMemo(() => {
+    // fixme - rename optOut options to "enabled" - opt out is confusing when the settings are "enabled"
+    // leads to trying to think about the opposite of what the setting conveys
     return new Analytics({
-      optOut: !(OsContextSingleton?.isTelemetryEnabled() ?? true),
+      optOut: !(telemetryEnabled ?? true),
       crashReportingOptOut: !(
-        (OsContextSingleton?.isCrashReportingEnabled() ?? true) &&
+        (crashReportingEnabled ?? true) &&
         typeof process.env.SENTRY_DSN === "string"
       ),
       amplitudeApiKey: props.amplitudeApiKey ?? process.env.AMPLITUDE_API_KEY,
