da35cc9cd71066b1ef3210f4a8c71f6b60e538de
Roman Shtylman <roman@foxglove.dev>
2021-05-02 09:31:43 -0700

    add note to OsContextAppConfigurationProvider for where to consider moving it

--- app/context/OsContextAppConfigurationProvider.tsx
+++ app/context/OsContextAppConfigurationProvider.tsx
@@ -2,6 +2,9 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+// fixme - move this to renderer - this app configuration provider is specific to using preload stuff
+// we could even have preload expose different "contexts" for it
+
 import { PropsWithChildren, useMemo } from "react";
 
 import OsContextSingleton from "@foxglove-studio/app/OsContextSingleton";
